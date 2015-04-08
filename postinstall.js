6b3e3bdd7e540a65b0107bc5ad22f66584fe4d7a
Patrick Heneise <patrick@fastmail.im>
2015-04-08 13:15:44 +0200

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,3 @@
 'use strict';
 
-console.log(chalk.cyan('\n\nPlease run `sudo gem install sass dnode` if you don\'t have it installed already'));
+console.log('\n\nPlease run `sudo gem install sass dnode` if you don\'t have it installed already');
